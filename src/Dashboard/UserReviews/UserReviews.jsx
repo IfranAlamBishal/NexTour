11af8a27c3a867b9b41227e22dd9c05e4525958f
Your NameIfranAlamBishal <ifranalam4089@gmail.com>
2024-11-10 23:35:32 +0600

    update: modification done on user review page.

--- src/Dashboard/UserReviews/UserReviews.jsx
+++ src/Dashboard/UserReviews/UserReviews.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
 import { Helmet } from "react-helmet-async";
 import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
 import useUserBookingData from "../../Hooks/useUserBookingData";
+import { Link } from "react-router-dom";
 
 const UserReviews = () => {
     const { user } = useContext(AuthContext);
@@ -46,8 +47,47 @@ const UserReviews = () => {
 
                     <div className=" w-5/6 mx-auto my-20">
 
-                    <h1 className=" text-2xl font-semibold text-orange-500 my-10"> Total Tours to Review: {verifiedBookings.length} </h1>
-                        <h1 className=" text-4xl font-semibold text-orange-500 text-center my-40"> No Tours to review</h1>
+                        <h1 className=" text-2xl font-semibold text-orange-500 my-10"> Total Tours to Review: {verifiedBookings.length} </h1>
+                        <div>
+                            {
+                                verifiedBookings.length > 0 ?
+                                    <>
+                                        {/* Table */}
+                                        <div className="overflow-x-auto  my-5">
+                                            <table className="table table-zebra">
+                                                <thead>
+                                                    <tr className=" bg-orange-500 text-white text-base">
+                                                        <th></th>
+                                                        <th>Tour</th>
+                                                        <th>Review</th>
+                                                        <th className=" opacity-0">Update</th>
+                                                        <th className=" opacity-0">Remove</th>
+                                                    </tr>
+                                                </thead>
+                                                {/* <tbody>
+                                                    {
+                                                        availableBookings.map((booking, index) => <tr key={booking._id} className=" text-base">
+                                                            <th>{index + 1}</th>
+                                                            <td>{booking.tourists_spot_name}</td>
+                                                            <td>{booking.trx_Id}</td>
+                                                            <td>{booking.totalCost}</td>
+                                                            <td>{booking.status}</td>
+
+                                                            <td><Link onClick={() => handleRemove(booking)} className=" btn bg-orange-500 text-white">Cancel</Link></td>
+
+                                                        </tr>)
+                                                    }
+                                                </tbody> */}
+                                            </table>
+                                        </div>
+                                    </>
+                                    :
+                                    <>
+                                        <h1 className=" text-4xl font-semibold text-orange-500 text-center my-40"> No Tours to review</h1>
+                                    </>
+                            }
+                        </div>
+
                     </div>
 
                 </div>
